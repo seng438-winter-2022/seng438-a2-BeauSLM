3d949bad757194d73aba1d7a37d75ddc176cba30
Apostolos Scondrianis <apostolos.scondriani@ucalgary.ca>
2022-02-07 22:06:43 -0700

    change in proposal, change in datautilities test

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -1,15 +1,28 @@
 package org.jfree.data.test;
+import org.jfree.data.Range;
 
 import static org.junit.Assert.*;
 
 import org.jfree.data.DataUtilities;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
-public class DataUtilitiesTest extends DataUtilities {
+public class DataUtilitiesTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    @Before
+    public void setUp() throws Exception { 
+
+    }
+
+    
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
     }
 
 }
