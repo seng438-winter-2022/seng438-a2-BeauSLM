cf437a6730a0d5df30b8ef6eb463aea2fb512483
Apostolos Scondrianis <apostolos.scondriani@ucalgary.ca>
2022-02-07 20:46:38 -0700

    Comments for shift Tests

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -12,14 +12,13 @@ public class RangeTest {
     	exampleRange = new Range(-1, 1);
     }
 
-    //Shift Tests Starts
     @Test
     public void centralValueShouldBeZero() {
         assertEquals("The central value of -1 and 1 should be 0",
         0, exampleRange.getCentralValue(), .000000001d);
     }
     
-    
+    //Shift Test
     @Test
     public void NegativeShiftValueLowerBoundCheck() {
     	Range shiftRange = new Range(-2, 2);
@@ -27,7 +26,8 @@ public class RangeTest {
     	Range.shift(shiftRange, delta);
     	assertEquals("The lower bound of -2 and 2 after shift of -2.5 should be -4.5", -4.5, shiftRange.getLowerBound(), 0.1);
     }
-
+    
+    //Shift Test
     @Test
     public void negativeShiftValueUpperBoundCheck() {
     	Range shiftRange = new Range(-2, 2);
@@ -36,6 +36,7 @@ public class RangeTest {
     	assertEquals("The uppper bound of -2 and 2 after shift of -2.5 should be -0.5", -0.5, shiftRange.getUpperBound(), 0.1);
     }
 
+    //Shift Test
     @Test
     public void positiveShiftValueLowerBoundCheck() {
     	Range shiftRange = new Range(-2, 2);
@@ -44,6 +45,7 @@ public class RangeTest {
     	assertEquals("The lower bound of -2 and 2 after shift of 2.0 should be 0", 0, shiftRange.getLowerBound(), 0.1);
     }
 
+    //Shift Test
     @Test
     public void positiveShiftValueUpperBoundCheck() {
     	Range shiftRange = new Range(-2, 2);
@@ -52,7 +54,7 @@ public class RangeTest {
     	assertEquals("The upper bound of -2 and 2 after shift of 2 should be 4.0", 4.0, shiftRange.getUpperBound(), 0.1);
     }
     
-
+    //Shift Test
     @Test
     public void zeroShiftValueLowerBoundCheck() {
     	Range shiftRange = new Range(-2, 2);
@@ -61,6 +63,7 @@ public class RangeTest {
     	assertEquals("The lower bound of -2 and 2 after shift of 0 should be -2.0", -2.0, shiftRange.getLowerBound(), 0.1);
     }
 
+    //Shift Test
     @Test
     public void zeroShiftValueUpperBoundCheck() {
     	Range shiftRange = new Range(-2, 2);
@@ -69,13 +72,13 @@ public class RangeTest {
     	assertEquals("The upper bound of -2 and 2 after shift of 0 should be 2.0", 2.0, shiftRange.getUpperBound(), 0.1);
     }
     
+    //Shift Test
     @Test(expected = IllegalArgumentException.class)
     public void nullRangeShiftCheck() {
     	Range shiftRange = null;
     	double delta = 2.0;
     	Range.shift(shiftRange, delta);
     }
-    //Shift Tests end
     
     //toString test
     @Test public void toStringCheck() {
