13a2ea026ac3ba58bae7b4d71ea567c261e945b4
Beau McCartney <mccartney.beausl@gmail.com>
2022-02-08 22:25:37 -0700

    Add calculate row/col total for three values test

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -203,6 +203,53 @@ public class DataUtilitiesTest {
 	, expected, actual);
 	}
     
+    @Test
+    public void calculateRowTotalForThreeValues() {
+        // setup
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(3));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(2.5));
+            }
+        });
+        // exercise 
+        double result = DataUtilities.calculateRowTotal(values, 0);
+        // verify
+        assertEquals(result, 12.5, .000000001d);
+        // tear-down: NONE in this test method
+    }
+    
+    @Test
+    public void calculateColumnTotalForThreeValues() {
+        // setup
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(3));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(1, 0);
+                will(returnValue(2.5));
+                one(values).getValue(2, 0);
+                will(returnValue(2.5));
+            }
+        });
+        // exercise 
+        double result = DataUtilities.calculateColumnTotal(values, 0);
+        // verify
+        assertEquals(result, 12.5, .000000001d);
+        // tear-down: NONE in this test method
+    }
 
     @After
     public void tearDown() throws Exception {
