54f98618e254d458080a4b804ed42389d1a9e2ed
Beau McCartney <mccartney.beausl@gmail.com>
2022-02-08 22:23:47 -0700

    Test calculate row/col total with valid values

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -78,7 +78,31 @@ public class DataUtilitiesTest {
         // tear-down: NONE in this test method
     }
     
-
+    @Test
+    public void calculateColumnTotalForTwoValidRows() {
+        // setup
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(3));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(1, 0);
+                will(returnValue(2.5));
+                one(values).getValue(2, 0);
+                will(returnValue(2.5));
+            }
+        });
+        // exercise
+        int []arr = { 0, 2 };
+        double result = DataUtilities.calculateColumnTotal(values, 0, arr);
+        // verify
+        assertEquals(result, 10.0, .000000001d);
+        // tear-down: NONE in this test method
+    }
+    
     @Test
     public void calculateRowTotalForTwoValues() {
         // setup
@@ -100,6 +124,32 @@ public class DataUtilitiesTest {
         assertEquals(result, 10.0, .000000001d);
         // tear-down: NONE in this test method
     }
+    
+    @Test
+    public void calculateRowTotalForTwoValidColumns() {
+        // setup
+        Mockery mockingContext = new Mockery();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(3));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(2.5));
+            }
+        });
+        // exercise
+        int []arr = { 0, 2 };
+        double result = DataUtilities.calculateRowTotal(values, 0, arr);
+        // verify
+        assertEquals(result, 10.0, .000000001d);
+        // tear-down: NONE in this test method
+    }
+    
 
     @After
     public void tearDown() throws Exception {
